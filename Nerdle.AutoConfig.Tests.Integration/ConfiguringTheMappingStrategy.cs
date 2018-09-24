2f5cee844b5a7757e5c38efc17c78f32cb38cbdc
Cédric Luthi <cedric.luthi@gmail.com>
2018-09-25 00:13:15 +0200

    Add tests for configuring inherited types

--- Nerdle.AutoConfig.Tests.Integration/ConfiguringTheMappingStrategy.cs
+++ Nerdle.AutoConfig.Tests.Integration/ConfiguringTheMappingStrategy.cs
@@ -70,6 +70,50 @@ namespace Nerdle.AutoConfig.Tests.Integration
             config.Baz.Should().Be("baz");
             config.Qux.Should().Be("From custom mapper");
         }
+
+        [Test]
+        public void Configuring_the_mapping_of_inherited_interfaces()
+        {
+            AutoConfig.WhenMapping<IBaseConfiguration>(mapper =>
+            {
+                mapper.Map(x => x.MyString).OptionalWithDefault("base");
+                mapper.Map(x => x.MyInt).OptionalWithDefault(42);
+            });
+            AutoConfig.WhenMapping<IDerivedConfiguration>(mapper =>
+            {
+                mapper.Map(x => x.MyString).OptionalWithDefault("derived");
+            });
+            
+            var baseConfig = AutoConfig.Map<IBaseConfiguration>(configFilePath: ConfigFilePath);
+            baseConfig.MyString.Should().Be("base");
+            baseConfig.MyInt.Should().Be(42);
+            
+            var derivedConfig = AutoConfig.Map<IDerivedConfiguration>(configFilePath: ConfigFilePath);
+            derivedConfig.MyString.Should().Be("derived");
+            derivedConfig.MyInt.Should().Be(42);
+        }
+
+        [Test]
+        public void Configuring_the_mapping_of_inherited_classes()
+        {
+            AutoConfig.WhenMapping<BaseConfiguration>(mapper =>
+            {
+                mapper.Map(x => x.MyString).OptionalWithDefault("base");
+                mapper.Map(x => x.MyInt).OptionalWithDefault(42);
+            });
+            AutoConfig.WhenMapping<DerivedConfiguration>(mapper =>
+            {
+                mapper.Map(x => x.MyString).OptionalWithDefault("derived");
+            });
+            
+            var baseConfig = AutoConfig.Map<BaseConfiguration>(configFilePath: ConfigFilePath);
+            baseConfig.MyString.Should().Be("base");
+            baseConfig.MyInt.Should().Be(42);
+            
+            var derivedConfig = AutoConfig.Map<DerivedConfiguration>(configFilePath: ConfigFilePath);
+            derivedConfig.MyString.Should().Be("derived");
+            derivedConfig.MyInt.Should().Be(42);
+        }
     }
 
     public interface ICustomConfiguration
@@ -78,6 +122,28 @@ namespace Nerdle.AutoConfig.Tests.Integration
         string Bar { get; }
         string Baz { get; }
         string Qux { get; }
+    }
+
+    public interface IBaseConfiguration
+    {
+        string MyString { get; }
+        int MyInt { get; }
+    }
+
+    public class BaseConfiguration
+    {
+        public string MyString { get; set; }
+        public int MyInt { get; set; }
+    }
+    
+    public interface IDerivedConfiguration : IBaseConfiguration
+    {
+        new string MyString { get; }
+    }
+
+    public class DerivedConfiguration : BaseConfiguration
+    {
+        public new string MyString { get; set; }
     }
 
     class CustomMapper : IMapper
